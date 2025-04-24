56940da1b2242fbf5b60676710c6ccbd54201671
Umar Solanki <uysolanki@gmail.com>
2025-04-24 08:36:07 +0530

    string reverse

---
+++ src/day8scanner/ScannerDemo.java
@@ -0,0 +1,56 @@
+package day8scanner;
+
+import java.util.Scanner;
+
+public class ScannerDemo {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter your height in feets");   //5.11
+	int height=(int)sc.nextDouble();
+
+	System.out.println(height);
+//	if(runs==6)
+//	{
+//		System.out.println("If its in the arc, its out of the park");
+//	}
+//	
+//	if(runs==4)
+//	{
+//		System.out.println("Square drive to the boundry");
+//	}
+	
+	
+//	System.out.println("Decision pending..."); 
+//	
+//	System.out.println("Picthing?  [1-OFF, 2-Center ,3-Leg]"); 
+//	int pitching=sc.nextInt();
+//	
+//	if(pitching==3)
+//	{
+//		System.out.println("NOT OUT");
+//		System.exit(0);
+//	}
+//	
+//	System.out.println("Impact?    [Within Line 1, Outside the line 2]"); 
+//	int impact=sc.nextInt();
+//	if(impact==2)
+//	{
+//		System.out.println("NOT OUT");
+//		System.exit(0);
+//	}
+//	
+//	System.out.println("Hitting Wickets?  Yes 1 , No 2"); 
+//	int wicketshitting=sc.nextInt();
+//	
+//	if(  (pitching==1 || pitching==2) && (impact==1) && (wicketshitting==1))
+//	{
+//		System.out.println("OUT");
+//	}
+//	else
+//	{
+//		System.out.println("NOT OUT");
+//	}
+//	
+	
+}
+}
---
+++ src/day8scanner/StringReverse.java
@@ -0,0 +1,73 @@
+package day8scanner;
+
+public class StringReverse {
+
+	public static void main(String[] args) {
+		String name="mmmaazkzaammm";
+//		StringBuffer buffer=new StringBuffer(name);   without using predefined function
+//		System.out.println(buffer.reverse());
+		
+//		String revName="";							//without using second string
+		int loopCounter=0;
+//		for(int i=name.length()-1;i>=0;i--)
+//		{
+//			 loopCounter++;
+//			revName=revName+name.charAt(i);
+//		}
+//		
+//		System.out.println(revName);
+//		if(name.compareTo(revName)==0)
+//			System.out.println("Palindrome");
+//		else
+//			System.out.println("Not Palindrome");
+//		System.out.println("Number of Iteration "+loopCounter);
+																					//j
+			//T(n) = O(n)  Linear Time Complexity														//i     
+																	//0     1    2    3
+//		char[] str=name.toCharArray();								//D     E    M    O
+//		int i=0;
+//		int j=str.length-1;
+//		
+//		while(i<=j)
+//		{
+//			char temp=str[i];											//temp= 'A'
+//			str[i]=str[j];
+//			str[j]=temp;
+//			i++;
+//			j--;
+//		}
+//		
+//		System.out.println(str);
+		
+		
+		char[] str=name.toCharArray();
+		int i=0;
+		int j=str.length-1;
+		int flag=0; //assuming Palindrome String
+		while(i<=j)
+		{
+			 loopCounter++;
+			if(str[i]!=str[j])
+			{
+				flag=1;   //flag being set to 1 means its not a palindrome
+				break;
+			}
+			i++;
+			j--;
+		}
+		
+		if(flag==0)
+			System.out.println("Palindrome!!!!!");
+		else
+			System.out.println("Not Palindrome!!!!!");
+		
+		System.out.println("Number of Iteration "+loopCounter);
+		
+		//T(n) = O(n/2)  //130000   65000x2
+	    //Asymptotic Notation big O  //worst case		
+	}
+
+}
+
+
+//mom
