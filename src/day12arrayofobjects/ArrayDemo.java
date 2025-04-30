1a8f9ae0063075ca1ef8195dc6e2ce196fe947e1
Umar Solanki <uysolanki@gmail.com>
2025-04-30 19:59:04 +0530

    array day 1

---
+++ src/day12arrayofobjects/ArrayDemo.java
@@ -0,0 +1,41 @@
+package day12arrayofobjects;
+
+import java.util.Scanner;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+//		int ages[]=new int[5];   //array declaration
+//
+//		Scanner sc=new Scanner(System.in);
+//		
+//		for(int i=0;i<ages.length;i++)  //array initialization
+//		{
+//			System.out.println("Enter a Number");  //17
+//			ages[i]=sc.nextInt();	
+//		}
+//		System.out.println("The ages entered are as follows");
+//		for(int i=0;i<ages.length;i++)
+//		{
+//			System.out.println(ages[i]);  //17	
+//		}
+//		
+//		int sum=0;
+//		for(int i=0;i<ages.length;i++)
+//		{
+//			sum+=ages[i];
+//		}
+//		
+//		double avgAge=sum/ages.length;
+//		System.out.println("Average age is "+avgAge);
+//		
+		int rno[]= {17,1,'a',1,1}; //declaration + initialization
+		
+		for(int i=0;i<rno.length;i++)
+		{
+			System.out.println(rno[0]);  //17	
+		}
+		
+	}
+
+}
---
+++ src/day12arrayofobjects/DriverApp.java
@@ -0,0 +1,16 @@
+package day12arrayofobjects;
+
+import day11oops.Mobile;
+
+public class DriverApp {
+
+	public static void main(String[] args) {
+		Mobile m1=new Mobile(103,"Apple","Ipone16 Pro Max",135000.0);  
+		
+		Mobile m2=new Mobile(101, "Samsung", "S24 Ultra", 75000.0);
+		
+		Mobile m3=new Mobile(102, "One Plus", "12 pro", 55000.0);
+
+	}
+
+}
